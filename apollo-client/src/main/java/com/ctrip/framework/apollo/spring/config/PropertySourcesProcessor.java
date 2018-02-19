6528a7e1b7cc69a8fc24e08f37c87dabb146afab
nobodyiam <nobodyiam@gmail.com>
2018-02-19 21:41:59 +0800

    refactor

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/config/PropertySourcesProcessor.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/config/PropertySourcesProcessor.java
@@ -1,9 +1,6 @@
 package com.ctrip.framework.apollo.spring.config;
 
-import com.google.common.collect.HashMultimap;
-import com.ctrip.framework.apollo.ConfigChangeListener;
 import com.google.common.collect.ImmutableSortedSet;
-import com.google.common.collect.Lists;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 
@@ -22,7 +19,6 @@ import org.springframework.core.env.Environment;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * Apollo Property Sources processor for Spring Annotation Based Application. <br /> <br />
@@ -35,19 +31,12 @@ import java.util.List;
  * @author Jason Song(song_s@ctrip.com)
  */
 public class PropertySourcesProcessor implements BeanFactoryPostProcessor, EnvironmentAware, PriorityOrdered {
-  private static final List<Config> ALL_CONFIG = Lists.newLinkedList();
   private static final Multimap<Integer, String> NAMESPACE_NAMES = LinkedHashMultimap.create();
 
   private ConfigurableEnvironment environment;
 
   public static boolean addNamespaces(Collection<String> namespaces, int order) {
     return NAMESPACE_NAMES.putAll(order, namespaces);
-  }
-
-  public static void registerListener(ConfigChangeListener configChangeListener){
-    for(Config config:ALL_CONFIG){
-      config.addChangeListener(configChangeListener);
-    }
   }
 
   @Override
@@ -70,7 +59,6 @@ public class PropertySourcesProcessor implements BeanFactoryPostProcessor, Envir
       int order = iterator.next();
       for (String namespace : NAMESPACE_NAMES.get(order)) {
         Config config = ConfigService.getConfig(namespace);
-        ALL_CONFIG.add(config);
         composite.addPropertySource(new ConfigPropertySource(namespace, config));
       }
     }
